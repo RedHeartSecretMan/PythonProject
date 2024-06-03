5863f116efe827de734426aa72ca79106f2cf7a0
WangHao <1046911101@qq.com>
2024-06-03 16:32:38 +0800

    [Normal]: 20240603

--- test/test_chattts/faststart.py
+++ test/test_chattts/faststart.py
@@ -1,18 +1,18 @@
 import os
 import sys
 
+import sounddevice as sd
+import torch
+
 sys.path.append(
     os.path.abspath(
         os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "ChatTTS")
     )
 )
-import sounddevice as sd
-import torch
-
 import ChatTTS
 
-torch._dynamo.config.cache_size_limit = 64
-torch._dynamo.config.suppress_errors = True
+torch._dynamo.config.cache_size_limit = 64  # type: ignore
+torch._dynamo.config.suppress_errors = True  # type: ignore
 torch.set_float32_matmul_precision("high")
 
 chat = ChatTTS.Chat()
@@ -20,5 +20,6 @@ chat.load_models(source="local", local_path="chattts/resources/nosft")
 
 texts = ["中午好，我的朋友ChatTTS。"]
 
-wavs = chat.infer(texts, use_decoder=True)
-sd.play(wavs[0], samplerate=24000)
+wavs = chat.infer(texts)
+sd.play(wavs[0].transpose(1, 0), samplerate=24000)
+sd.wait()
