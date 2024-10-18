6878b49462cfbf6fb353c52b375391adf3d4727c
WangHao <1046911101@qq.com>
2024-10-18 15:02:00 +0800

    [Normal]: 20241018

--- test/test_minicpm/faststart.py
+++ test/test_minicpm/faststart.py
@@ -1,22 +1,28 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
+from transformers import AutoTokenizer
+from vllm import LLM, SamplingParams
 
-torch.manual_seed(51)
 
-path = "./stores/minicpm3/4b"
-tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(
-    path, torch_dtype=torch.bfloat16, device_map="cuda", trust_remote_code=True
-)
-
+model_name = "./stores/minicpm3/4b"
 texts = """女子提分手竟遭男友杀害嫌犯自杀未果报警自首
            请以故事的形式还原案件发展过程尽量不要平铺直述、矛盾点、冲击点可以场景化，侧重人物心理变化描写
            字数2000左右，三到四行一段，每段间隔一行"""
-responds, history = model.chat(
-    tokenizer,
-    texts,
-    temperature=0.7,
-    top_p=0.7,
-    max_new_tokens=8192*4,
+prompt = [
+    {
+        "role": "user",
+        "content": f"{texts}",
+    }
+]
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+input_text = tokenizer.apply_chat_template(
+    prompt, tokenize=False, add_generation_prompt=True
 )
-print(responds)
+
+llm = LLM(
+    model=model_name, trust_remote_code=True, tensor_parallel_size=1, max_model_len=5500
+)
+sampling_params = SamplingParams(top_p=0.7, temperature=0.7, max_tokens=1024)
+
+outputs = llm.generate(prompts=str(input_text), sampling_params=sampling_params)
+
+print(outputs[0].outputs[0].text)
