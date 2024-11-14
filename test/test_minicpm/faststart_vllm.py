1af1908e1384d60e2bf914102463f51e55e8572d
WangHao <1046911101@qq.com>
2024-11-14 14:35:07 +0800

    [Normal]: 20241114

--- test/test_minicpm/faststart_vllm.py
+++ test/test_minicpm/faststart_vllm.py
@@ -3,9 +3,8 @@ from vllm import LLM, SamplingParams
 
 
 model_name = "./stores/minicpm3/4b"
-texts = """女子提分手竟遭男友杀害嫌犯自杀未果报警自首
-           请以故事的形式还原案件发展过程尽量不要平铺直述、矛盾点、冲击点可以场景化，侧重人物心理变化描写
-           字数2000左右，三到四行一段，每段间隔一行"""
+texts = """介绍5个北京的景点。
+           灵活运用多种修辞手法，字数800左右。"""
 prompt = [
     {
         "role": "user",
@@ -24,5 +23,4 @@ llm = LLM(
 sampling_params = SamplingParams(top_p=0.7, temperature=0.7, max_tokens=1024)
 
 outputs = llm.generate(prompts=str(input_text), sampling_params=sampling_params)
-
 print(outputs[0].outputs[0].text)
